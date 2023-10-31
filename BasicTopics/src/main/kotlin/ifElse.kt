957c633672a83bb71831c9dbb85c5e10799ceef9
Pritom Debnath <debnathpritom@outlook.com>
2023-11-01 02:02:44 +0600

    Created BasicTopic

---
+++ BasicTopics/src/main/kotlin/ifElse.kt
@@ -0,0 +1,23 @@
+class ifElse {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            var num = 10
+            // In case of handeling conditional problem we can use If-else, lets see an example, a problem of finding the odd numbers.
+            if(num > 0) {
+                print("Positive number")
+            } else if( num < 0){
+                print("Negative number")
+            } else {
+                print("Non-negative number")
+            }
+            // In case we have one line after the if statement we can ignore using braces.
+
+            if(num > 0) print("Positive number")
+            else if( num < 0)   print("Negative number")
+            else    print("Non-negative number")
+
+
+        }
+    }
+}
