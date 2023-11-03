df41c1a33f62da64e7beba772dca2f9553393673
Pritom Debnath <debnathpritom@outlook.com>
2023-11-03 19:53:26 +0600

    Added FunctionIntro.kt: Introduction to function in kotlin
    Co-Authored-By: Wasik Ahmed Apon <22-47698-2@student.aiub.com>

---
+++ BasicTopics/src/main/kotlin/Function/Intro/FunctionIntro.kt
@@ -0,0 +1,12 @@
+package Function.Exercises
+
+class functionIntro {
+    //A function is a block of code which only runs when it is called.
+    //You can pass data, known as parameters, into a function.
+    //Functions are used to perform certain actions, and they are also known as methods.
+//    Predefined Functions :
+//    So it turns out you already know what a function is. You have been using it the whole time through this tutorial!
+//    For example, println() is a function. It is used to output/print text to the screen:
+
+
+}
